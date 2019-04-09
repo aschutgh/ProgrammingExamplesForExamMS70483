a85bee422415c3fcb25afb80a96b2a5d857cfbb2
Arthur Schut <aschutgh@gmail.com>
2019-04-09 14:17:06 +0200

    Added switch example

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = "connection_failed";
+            // string response = "connection_succes";
+            // string response = "this will default";
+
+            switch(response)
+            {
+                case "connection_failed":
+                    Console.WriteLine("The connection failed!");
+                    break;
+                case "connection_succes":
+                    Console.WriteLine("The connection succeeded.");
+                    break;
+                default:
+                    Console.WriteLine("This message prints if above cases fail");
+                    break;
+            }
+
+            Console.WriteLine("===============================");
+            // switch fall through example
+            int value = 5;
+
+            switch(value)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    Console.WriteLine("Variable value has value 0, 1 or 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Variable value has value 3");
+                    break;
+                default:
+                    Console.WriteLine("Variable value has value larger than 3");
+                    break;
+            }
+        }
+    }
+}
