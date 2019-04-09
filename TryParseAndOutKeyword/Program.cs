4ebf86643f4fdd2ea18df69c46b469af28508ade
Arthur Schut <aschutgh@gmail.com>
2019-04-09 14:01:44 +0200

    Added TryParse example

---
+++ TryParseAndOutKeyword/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TryParseAndOutKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string numberString = "1234";
+            // comment line above and uncomment line below to fail
+            // string numberString = "1234thisshouldfail";
+            // try to convert numberString to int and place result in number
+            // returns true if succes otherwise false
+            if (int.TryParse(numberString, out number)) // out keyword
+            {
+                Console.WriteLine("Conversion succeeded!");
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+        }
+    }
+}
