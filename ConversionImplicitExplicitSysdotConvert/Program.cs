23248688b4c3de83ab907ce20e9fd86ff044791d
Arthur Schut <aschutgh@gmail.com>
2019-04-09 13:53:38 +0200

    Added example about implicit and explicit conversion and using System.Convert

---
+++ ConversionImplicitExplicitSysdotConvert/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConversionImplicitExplicitSysdotConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion
+            int a = 4;
+            long b;
+            b = a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            // explicit conversion
+            Console.WriteLine("================================");
+            int c;
+            long d = 5;
+            c = (int)d; // explicit conversion of long d to int c
+            Console.WriteLine(c);
+
+            // using System.Convert()
+
+            string possibleInt = "1234";
+            int count = Convert.ToInt32(possibleInt);
+            Console.WriteLine(count);
+        }
+    }
+}
