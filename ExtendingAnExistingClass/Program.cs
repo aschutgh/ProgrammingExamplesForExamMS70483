9ed35a79effcbbb334e7efd981ec82ef2d5e02f8
Arthur Schut <aschutgh@gmail.com>
2019-04-09 13:40:10 +0200

    Added extending an exising class example

---
+++ ExtendingAnExistingClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Derive a new class from an existing class
+ * Add or override methods
+ * 
+ */
+
+
+namespace ExtendingAnExistingClass
+{
+    public class UniqueList<T> : List<T>
+    {
+        public void RemoveDuplicates()
+        {
+            base.Sort();
+            for (int i = this.Count - 1; i > 0; i--)
+            {
+                if (this[i].Equals(this[i - 1]))
+                    this.RemoveAt(i);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ul = new UniqueList<int>();
+            ul.Add(1);
+            ul.Add(2);
+            ul.Add(3);
+            ul.Add(3);
+            foreach(int i in ul)
+            {
+                Console.Write($"{i} ");
+            }
+            ul.RemoveDuplicates();
+            Console.WriteLine();
+            Console.WriteLine("After removing duplicates:");
+            foreach (int i in ul)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
