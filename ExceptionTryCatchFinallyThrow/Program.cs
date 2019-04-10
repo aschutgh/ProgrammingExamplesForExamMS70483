b3655d96d720c5bb059c52b6f37d1e2c42da37b1
Arthur Schut <aschutgh@gmail.com>
2019-04-10 12:55:38 +0200

    Added project for exception example

---
+++ ExceptionTryCatchFinallyThrow/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/*
+ * FIXME:
+ * Add exception handling
+ * use the following:
+ * try / catch
+ * try / catch / finally
+ * try / finally
+ * throw keyword
+ */
+
+namespace ExceptionTryCatchFinallyThrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
