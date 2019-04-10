5c3eb625d5217b3446f466afdfe16f3cd13b573c
Arthur Schut <aschutgh@gmail.com>
2019-04-10 09:48:44 +0200

    Added a custom constructor

--- StructWithIndexer/Program.cs
+++ StructWithIndexer/Program.cs
@@ -12,16 +12,22 @@ namespace StructWithIndexer
             public int Strength;
             public string Bean;
             public string CountryOfOrigin;
+
+            // Name of the constructor is equal to the name of the struct
+            public Coffee(int strength, string bean, string countryOfOrigin )
+            {
+                this.Strength = strength; // this refers to the particular instance of a Coffee struct
+                this.Bean = bean; // bean refers to the parameter in the custom Coffee constructor
+                this.CountryOfOrigin = countryOfOrigin;
+            }
         }
 
 
         static void Main(string[] args)
         {
             // Using a struct
-            Coffee coffee1 = new Coffee();
-            coffee1.Strength = 3;
-            coffee1.Bean = "Arabica";
-            coffee1.CountryOfOrigin = "Kenya";
+            Coffee coffee1 = new Coffee(3, "Arabica", "Kenya");
+            
             Console.WriteLine(coffee1.Strength);
             Console.WriteLine(coffee1.Bean);
             Console.WriteLine(coffee1.CountryOfOrigin);
