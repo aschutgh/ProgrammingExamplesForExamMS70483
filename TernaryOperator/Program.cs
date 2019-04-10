d20a602b934c77db0bf01b4d1470d2eed2d725c5
Arthur Schut <aschutgh@gmail.com>
2019-04-10 16:05:03 +0200

    Added ternary operator example

--- TernaryOperator/Program.cs
+++ TernaryOperator/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
 
 /*
- * FIXME: Add ternary operator example
+ * source: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/conditional-operator
+ * 
+ * condition ? consequent : alternative
+ * 
+ * The condition expression must evaluate to true or false.
+ * If condition evaluates to true, the consequent expression is
+ * evaluated, and its result becomes the result of the operation.
+ * If condition evaluates to false, the alternative expression is
+ * evaluated, and its result becomes the result of the operation.
+ * 
  * 
  */
 
@@ -12,7 +21,10 @@ namespace TernaryOperator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i = -10; i <= 20; i++)
+            {
+                Console.WriteLine($"{i} is {(i % 2 == 0 ? "even" : "odd" )}");
+            }
         }
     }
 }
