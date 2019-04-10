c9ff0c335470f05270fb70641713b72ee3268b43
Arthur Schut <aschutgh@gmail.com>
2019-04-10 12:58:18 +0200

    Added project for regular expression example

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * FIXME: Add regular expression example
+ * 
+ */
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
