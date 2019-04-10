714a29674b1686cfa22addfab32757d965e60d1a
Arthur Schut <aschutgh@gmail.com>
2019-04-10 15:09:20 +0200

    Example added

--- NullCoalescingOperator/Program.cs
+++ NullCoalescingOperator/Program.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 
 /*
- * FIXME: Add example about the ?? null coalscing operator
- * 
+ * source: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-coalescing-operator
  */
 
-namespace NullCoalescingOperator
+class NullCoalescingOperator
 {
-    class Program
+    static int? GetNullableInt()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return null;
+    }
+
+    static string GetStringValue()
+    {
+        return null;
+    }
+
+    static void Main()
+    {
+        // int? allow variable x to be null
+        // reference types can be null
+        int? x = null;
+
+        // Set y to the value of x if x is NOT null; otherwise,
+        // if x == null, set y to -1.
+        int y = x ?? -1;
+        Console.WriteLine(y);
+
+        // Assign i to return value of the method if the method's result
+        // is NOT null; otherwise, if the result is null, set i to the
+        // default value of int.
+        int i = GetNullableInt() ?? default(int);
+        Console.WriteLine(i);
+
+        string s = GetStringValue();
+        // Display the value of s if s is NOT null; otherwise, 
+        // display the string "Unspecified".
+        Console.WriteLine(s ?? "Unspecified");
     }
 }
