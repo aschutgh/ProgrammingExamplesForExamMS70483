3f398a9f791df8cf80b467cb425aaedaa8d6741f
Arthur Schut <aschutgh@gmail.com>
2019-04-10 13:08:51 +0200

    Added project for partial class example

---
+++ PartialClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * FIXME: Add partial class example
+ * 
+ */
+
+namespace PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
