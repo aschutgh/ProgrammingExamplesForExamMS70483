e7e0597544fc00fd3e2c19a7d7057f4f39d406db
Arthur Schut <aschutgh@gmail.com>
2019-04-10 12:53:20 +0200

    Added project for array example

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/*
+ * FIXME: Add the following:
+ * - single dimensional arrays
+ * - multidimensional arrays
+ * - jagged arrays
+ * 
+ */
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
