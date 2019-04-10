1548b572742036cd21de69b9d9781a04f5625236
Arthur Schut <aschutgh@gmail.com>
2019-04-10 12:45:32 +0200

    Added project for Enum example

---
+++ Enum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// FIXME: Add Enum example
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
