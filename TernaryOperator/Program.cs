3344d3855af988e8723640efebd150fe9dea5b34
Arthur Schut <aschutgh@gmail.com>
2019-04-10 13:06:12 +0200

    Added project for ternary operator example

---
+++ TernaryOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/*
+ * FIXME: Add ternary operator example
+ * 
+ */
+
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
