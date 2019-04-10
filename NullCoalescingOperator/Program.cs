dea8e8ee3be8a1a51518bdceb4b0ff7f4213a121
Arthur Schut <aschutgh@gmail.com>
2019-04-10 13:01:28 +0200

    Added project for null coalescing operator example

---
+++ NullCoalescingOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+ * FIXME: Add example about the ?? null coalscing operator
+ * 
+ */
+
+namespace NullCoalescingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
