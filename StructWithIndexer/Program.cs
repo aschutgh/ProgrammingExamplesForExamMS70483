266ce70aa9c7d4549afcc6fd0b5c30f7b4e962d5
Arthur Schut <aschutgh@gmail.com>
2019-04-10 09:42:21 +0200

    Simple use of a struct

---
+++ StructWithIndexer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// source: 20483B Programming in Visual C#
+
+namespace StructWithIndexer
+{
+    class Program
+    {
+        //Declaring a struct
+        public struct Coffee
+        {
+            public int Strength;
+            public string Bean;
+            public string CountryOfOrigin;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Using a struct
+            Coffee coffee1 = new Coffee();
+            coffee1.Strength = 3;
+            coffee1.Bean = "Arabica";
+            coffee1.CountryOfOrigin = "Kenya";
+            Console.WriteLine(coffee1.Strength);
+            Console.WriteLine(coffee1.Bean);
+            Console.WriteLine(coffee1.CountryOfOrigin);
+        }
+    }
+}
