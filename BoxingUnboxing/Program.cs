59766beb780dc017211ca763e15f34e0c69e8d2a
Arthur Schut <aschutgh@gmail.com>
2019-04-10 14:54:52 +0200

    Boxing and unboxing examples

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/* 
+ * source: 20483B Programming in Visual C#
+ * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/boxing-and-unboxing
+ * 
+ */
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The process of converting a value type to a reference type is called boxing
+            int i = 100;
+            object o = i;
+            Console.WriteLine($"{i} {o}");
+            i = 133;
+            Console.WriteLine($"After changing variable i the values are: i: {i} and o: {o}");
+
+            // The process of converting a reference type to a value type is called unboxing.
+            Console.WriteLine("Unboxing:");
+            int j;
+            j = (int)o;
+            Console.WriteLine($"{o} {j}");
+
+        }
+    }
+}
