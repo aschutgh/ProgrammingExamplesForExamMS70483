6b2a2f6ce350f87020728787b9ab41531125c708
Arthur Schut <aschutgh@gmail.com>
2019-04-10 13:16:40 +0200

    Added project for recursion examples

---
+++ Recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/*
+ * Some recursion examples in C#
+ */
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
