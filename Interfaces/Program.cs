da1c75ace599233dee4c6e3b9b5c5e2f2b3ff3fd
Arthur Schut <aschutgh@gmail.com>
2019-04-18 16:02:39 +0200

    Added some interface examples

---
+++ Interfaces/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+// source: 20483B Programming in Visual C#
+
+namespace Interfaces
+{
+    // defining an interface
+    public interface ILoyaltyCardHolder
+    {
+        int TotalPoints { get; }
+        void AddPoints(decimal TransactionValue);
+        void ResetPoints();
+    }
+
+    public class Customer : ILoyaltyCardHolder
+    {
+        // the following implements the interface
+        private int totalPoints;
+
+        public int TotalPoints
+        {
+            get { return totalPoints; }
+        }
+
+        public void AddPoints(decimal transactionValue)
+        {
+            int points = Decimal.ToInt32(transactionValue);
+            totalPoints += points;
+        }
+
+        public void ResetPoints()
+        {
+            totalPoints = 0;
+        }
+        // Other members of the customer class
+        public string CustomerName { get; set; }
+        public int CustomerID { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ InterfacesCompare/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    class Order : IComparable
+    {
+        public DateTime Created { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Order o = obj as Order;
+
+            if (o == null)
+            {
+                throw new ArgumentException("Object is not an Order");
+            }
+
+            // Created is of type DateTime. DateTime objects are already comparable.
+            return this.Created.CompareTo(o.Created);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order { Created = new DateTime(2012, 12, 1 )},
+                new Order { Created = new DateTime(2012, 1, 6 )},
+                new Order { Created = new DateTime(2012, 7, 8 )},
+                new Order { Created = new DateTime(2012, 2, 20 )},
+            };
+            foreach (Order order in orders)
+            {
+                Console.WriteLine(order.Created);
+            }
+            orders.Sort();
+            Console.WriteLine("After sort: ");
+            foreach (Order order in orders)
+            {
+                Console.WriteLine(order.Created);
+            }
+        }
+    }
+}
