5b92540e080a6e9a357d496d958ad11b94eec2d2
Arthur Schut <aschutgh@gmail.com>
2019-04-18 16:02:59 +0200

    Added C Sharp directives example

---
+++ CSharpDirectives/Program.cs
@@ -0,0 +1,25 @@
+﻿#define DEBUG
+//#undef DEBUG
+using System;
+
+/*
+ * If DEBUG is defined some code runs. To disable this behaviour do the following:
+ * comment the #define DEBUG line
+ * uncomment the #undef DEBUG line
+ *  
+ *  source: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives/
+ */
+
+namespace CSharpDirectives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #if DEBUG
+                Console.WriteLine("Debug mode is on");
+            #endif            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
