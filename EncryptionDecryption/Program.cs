86f27e93775c4954214fabdf8fea2a6ca6d0a8c8
Arthur Schut <aschutgh@gmail.com>
2019-04-30 14:16:28 +0200

    Added encryption example

---
+++ EncryptionDecryption/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+/*
+ * source: Programming in C# Exam Ref 70-483
+ * Wouter de Kort
+ * 
+ */
+
+namespace EncryptionDecryption
+{
+    class Program
+    {
+        public static void EncryptSomeText()
+        {
+            string original = "My secret data!";
+
+            using (SymmetricAlgorithm symmetricAlgorithm = new AesManaged())
+            {
+                byte[] encrypted = Encrypt(symmetricAlgorithm, original);
+                string roundtrip = Decrypt(symmetricAlgorithm, encrypted);
+
+                // Displays: My secret data!
+                Console.WriteLine("Original:   {0}", original);
+                Console.WriteLine("Round Trip: {0}", roundtrip);
+            }
+        }
+
+        static byte[] Encrypt(SymmetricAlgorithm aesAlg, string plainText)
+        {
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using(CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return msEncrypt.ToArray();
+                }
+            }
+        }
+
+        static string Decrypt(SymmetricAlgorithm aesAlg, byte[] cipherText)
+        {
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            EncryptSomeText();
+        }
+    }
+}
