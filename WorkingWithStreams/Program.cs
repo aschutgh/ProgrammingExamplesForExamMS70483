64eeedba21e4d4da0095b5725204a991970f5b1e
Arthur Schut <aschutgh@gmail.com>
2019-05-02 14:09:39 +0200

    Added another stream example

--- WorkingWithStreams/Program.cs
+++ WorkingWithStreams/Program.cs
@@ -15,6 +15,7 @@ namespace WorkingWithStreams
         {
             /*
              * This only works when directory temp exists!
+             * Listing 4-14
              */
             string path = @"c:\temp\test.dat";
 
@@ -23,6 +24,18 @@ namespace WorkingWithStreams
                 string myValue = "MyValue";
                 byte[] data = Encoding.UTF8.GetBytes(myValue);
                 fileStream.Write(data, 0, data.Length);
+            }
+
+            /*
+             * Listing 4-15
+             */
+
+            string path2 = @"c:\temp\test2.dat";
+            // File.CreateText will create a text file with UTF-8 encoding
+            using (StreamWriter streamWriter = File.CreateText(path2))
+            {
+                string myValue = "MyValue";
+                streamWriter.Write(myValue);
             }
         }
     }
