6eb4e825e61f335318250baf9b6ad0845dcd612a
Arthur Schut <aschutgh@gmail.com>
2019-05-02 13:59:58 +0200

    Added first stream example

---
+++ WorkingWithStreams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+/*
+ * source: Programming in C# Exam Ref 70-483
+ * Wouter de Kort
+ */
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * This only works when directory temp exists!
+             */
+            string path = @"c:\temp\test.dat";
+
+            using (FileStream fileStream = File.Create(path))
+            {
+                string myValue = "MyValue";
+                byte[] data = Encoding.UTF8.GetBytes(myValue);
+                fileStream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
