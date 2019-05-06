8203a0bec3cf300b623efdcadfa339106d74e8cd
Arthur Schut <aschutgh@gmail.com>
2019-05-06 23:12:29 +0200

    Added async io example

---
+++ AsyncIO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+/*
+ * source: Programming in C# 70-483
+ * 
+ */
+
+namespace AsyncIO
+{
+    class Program
+    {
+        /*
+         * Listing 4-23
+         */
+        public static async Task CreateAndWriteAsyncToFile()
+        {
+            using (FileStream stream = new FileStream(@"c:\temp\asyncIOtest.dat", FileMode.Create, FileAccess.Write,
+                FileShare.None, 4096, true))
+            {
+                byte[] data = new byte[100000];
+                new Random().NextBytes(data);
+
+                await stream.WriteAsync(data, 0, data.Length);
+
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            await CreateAndWriteAsyncToFile();
+            
+        }
+    }
+}
